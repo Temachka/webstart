ab6c0022440cef6d4580d8fe4dd0a9d6e50c23d8
Artem <sashheka@gmail.com>
2019-08-05 12:13:09 +0300

    Исправил автозакрытие модального окна

--- src/js/main.js
+++ src/js/main.js
@@ -4,16 +4,11 @@ var close = document.querySelector('#close');
 
 button.addEventListener('click', function() {
   modal.classList.add('modal_active');
+  setTimeout(function() {
+    modal.classList.remove('modal_active');
+  }, 5000);
 });
 
-/* close.addEventListener('click', function() {
+close.addEventListener('click', function() {
   modal.classList.remove('modal_active');
-}); */
-
-/* setInterval(function() {
-  modal.classList.remove('modal_active');
-}, 5000); */
-
-setTimeout(function() {
-  modal.classList.remove('modal_active');
-}, 5000);
+});
