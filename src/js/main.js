419b9a6df62d97633e0fe21fbbfa8058f649988a
Artem <sashheka@gmail.com>
2019-08-05 12:52:22 +0300

    Теперь модально окно закрывается без проблем

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,37 @@
 var button = document.querySelector('#button');
 var modal = document.querySelector('#modal');
 var close = document.querySelector('#close');
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(function() {
+    modal.classList.remove('modal_active');
+  }, 5000);
+}
+
+function myStopFunction() {
+  clearTimeout(myVar);
+}
 
 button.addEventListener('click', function() {
   modal.classList.add('modal_active');
-  setTimeout(function() {
-    modal.classList.remove('modal_active');
-  }, 5000);
-});
+});  
 
 close.addEventListener('click', function() {
   modal.classList.remove('modal_active');
 });
+
+// button.addEventListener('click', function() {
+//   modal.classList.add('modal_active');
+//   timer = setTimeout(function() {
+//     modal.classList.remove('modal_active');
+//   }, 5000);
+// });
+
+// function stoptimer() {
+//   clearTimeout(timer);
+// }
+
+// close.addEventListener('click', function() {
+//   modal.classList.remove('modal_active');
+// });
