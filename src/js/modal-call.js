8ff0244941ba075c2ded21646318a0b60c2fd969
Artem <sashheka@gmail.com>
2019-08-11 11:13:01 +0300

    Добавил валидацию для всех форм

--- src/js/modal-call.js
+++ src/js/modal-call.js
@@ -2,17 +2,12 @@ $(document).ready(function() {
 var button_call = $('#button_call');
 var modal_call = $('#modal-call');
 var close_call = $('#close_call');
-var timeout;
 
   button_call.on('click', function() {
-    timeout = setTimeout(function() {
-      modal_call.removeClass('modal_active');
-    }, 5000);
     modal_call.addClass('modal_active');
   });
 
   close_call.on('click', function() {
-    clearTimeout(timeout);
     modal_call.removeClass('modal_active');
   });
 
